5b197d1ccd27b9788d853cdd45338b6e3f81defc
emaLeal <emanuel.leal@correounivalle.edu.co>
2025-05-01 15:49:51 -0500

    Requerimientos ajustados

--- DS1ProyectBackend/settings.py
+++ DS1ProyectBackend/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
-import dj_database_url
-import os
+# from decouple import config
+# import dj_database_url
+# import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,20 +91,20 @@ WSGI_APPLICATION = 'DS1ProyectBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(
-        config("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(
+#         config("DATABASE_URL"),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
 
 
 # Password validation
