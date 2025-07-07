5868578075436780e7e7204fdcb8c576840b49cf
cristian camilo cobo mendoza <ccobo1405@gmail.com>
2025-07-07 09:30:32 -0500

    cambios

--- job_offer/serializers.py
+++ job_offer/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from job_offer.models import JobOffer
 
 class JobOfferSerializer(serializers.ModelSerializer):
+    talent_director_name = serializers.ReadOnlyField(source='talent_director_document.name')
     class Meta:
         model = JobOffer
         fields = [
             'title', 'responsibilities', 'start_date', 'end_date',
             'education_level', 'job_type', 'rank', 'other_requirements',
-            'salary', 'status', 'talent_director_document'
+            'salary', 'status', 'talent_director_document','talent_director_name'
         ]
