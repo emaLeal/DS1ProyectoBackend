a1a775ee6d50bd69ef4f6eb87cdc0b3fcc4bb2f9
cristian camilo cobo mendoza <ccobo1405@gmail.com>
2025-07-07 16:25:43 -0500

    Editar oferta

--- job_offer/serializers.py
+++ job_offer/serializers.py
@@ -6,7 +6,7 @@ class JobOfferSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobOffer
         fields = [
-            'title', 'responsibilities', 'start_date', 'end_date',
+            'id','title', 'responsibilities', 'start_date', 'end_date',
             'education_level', 'job_type', 'rank', 'other_requirements',
             'salary', 'status', 'talent_director_document','talent_director_name'
         ]
